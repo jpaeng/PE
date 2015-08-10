7a66f7006a8158765bbf5fdaf56183ff6ada4292
jpaeng <jpaeng@gmail.com>
2015-08-10 15:32:35 -0700

    added p002 - works

--- p001.py
+++ p001.py
@@ -2,20 +2,21 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+
 def sum_multiples(max_n, n):
-    '''
+    """
     Sum all integers from 0 to max_n that are multiples of n
     Note:
         n + 2n + 3n + ...   = n(1+2+3+...)
         1 + 2 + 3 + ... + p = p*(p+1)/2
-    '''
-    p = int(max_n/n)     # integer division
-    return n*(p*(p+1)/2)
+    """
+    p = int(max_n / n)  # integer division
+    return n * (p * (p + 1) / 2)
+
 
 def solution(max_n):
-    return(sum_multiples(max_n, 3) + sum_multiples(max_n, 5) - sum_multiples(max_n, 15))
+    return (sum_multiples(max_n, 3) + sum_multiples(max_n, 5) - sum_multiples(max_n, 15))
 
 
 print(solution(9))
 print(solution(999))
-
--- test_pe.py
+++ test_pe.py
@@ -1,4 +1,15 @@
+
+import pytest
 import p001
+import p002
 
 def test_p001():
+    with pytest.raises(ZeroDivisionError):
+        p001.sum_multiples(100, 0)
+    assert p001.sum_multiples(9, 1)==45
+
     assert p001.solution(9) == 23
+
+def test_p002():
+    assert p002.solution(33) == 10
+    assert p002.solution(34) == 44
